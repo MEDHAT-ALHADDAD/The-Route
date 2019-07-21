525b32e69418855fdb5849a8577767036c8d53e4
Stealth-Phantom <project_4000@hotmail.com>
2019-07-21 20:49:01 +0200

    fixed js not working in home page

--- the_route/the_route/settings.py
+++ the_route/the_route/settings.py
@@ -71,6 +71,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'the_route.wsgi.application'
 
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static/'),
+)
+
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
