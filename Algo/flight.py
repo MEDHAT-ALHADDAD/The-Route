0d94a1d9e139c7f60145e7ca7e9f57c5947fd022
Mohamed Sameh <mohamedsameh1998@gmail.com>
2019-07-29 20:58:43 +0200

    Merge The algorithm with views.py

--- Algo/flight.py
+++ Algo/flight.py
@@ -8,3 +8,7 @@ class flight:
 
     def __str__(self):
         return "take trip from {} airport to {} airport on {} at price of {} from {} company".format(self.src,self.dst,self.date, self.price, self.carrier)
+
+    def getJSON(self, i):# i is the order of the flight in the list
+        #{"no":"1","From":"Cairo","To":"London","Flight_No":"@123","Date":"12/03/2020","Price":"500$"}
+        return {"no":i,"From":self.src,"To":self.dst,"Flight_No":"@123","Date":self.date,"Price":self.price}
