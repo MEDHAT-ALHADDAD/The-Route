914149af7c7bbbd966c0aff786cbc4267b7ba163
MEDHAT_ALHADDAD <medhat.alhaddad@gmail.com>
2019-07-29 15:44:04 +0200

    -f**k

--- the_route/website/urls.py
+++ the_route/website/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.Home, name="Home"),
-    path('Home/', views.Home, name = 'Home'),
+    path('Home/', views.Home, name = 'home'),
     path('register/', views.register, name = 'register'),
     path('rec/', views.rec, name = 'rec'),
     path('results/', views.results, name = 'results'),
