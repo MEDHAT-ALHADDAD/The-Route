84a9df337ddb6b1908241cab7ccb00af958a836d
Mohamed Sameh <mohamedsameh1998@gmail.com>
2019-07-30 13:33:31 +0200

    fixing merge

--- the_route/website/views.py
+++ the_route/website/views.py
@@ -65,7 +65,7 @@ def results(request):
         "results": res,
         "shortcity": city
         }
-        return JsonResponse(request,'request_page/Results.html',context)
+        return render(request,'request_page/Results.html',context)
     
     else:
         message = "Not Ajax"
